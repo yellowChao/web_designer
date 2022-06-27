34fe32a44a101b34842dbfb7b4b01ca8bf3dcb81
yellow <yellowchao91@163.com>
2022-06-27 18:00:57 +0800

    结合vant 完成上啦下拉加载

---
+++ vue2-final-demo/src/components/ArtItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <van-cell>
+      <!-- 标题区域的插槽 -->
+      <template #title>
+        <div class="title-box">
+          <!-- 标题 -->
+          <span>{{title}}</span>
+          <!-- 单张图片 -->
+          <img :src="cover.images[0]" alt="" class="thumb" v-if="cover.type === 1">
+        </div>
+        <!-- 三张图片 -->
+        <div class="thumb-box" v-if="cover.type === 3">
+          <img :src="item" alt="" class="thumb" v-for="item in cover.images">
+        </div>
+      </template>
+      <!-- label 区域的插槽 -->
+      <template #label>
+        <div class="label-box">
+          <span>作者: {{aut_name}} &nbsp;&nbsp; {{comm_count}}评论 &nbsp;&nbsp; 发布日期: {{pubdate}}</span>
+          <!-- 关闭按钮 -->
+          <van-icon name="cross" />
+        </div>
+      </template>
+    </van-cell>
+  </div>
+</template>
+
+<script>    
+    export default {
+       props: {
+        title: String,
+        aut_name: String,
+        comm_count: [String, Number], // 意思该字段可以有多种数据类型
+        pubdate: String,
+        cover: Object
+       }
+    }
+</script>
+
+
+<style lang="less" scoped>
+.label-box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.thumb {
+  // 矩形黄金比例：0.618
+  width: 113px;
+  height: 70px;
+  background-color: #f8f8f8;
+  object-fit: cover;
+}
+
+.title-box {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+
+.thumb-box {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- vue2-final-demo/src/views/Home.vue
+++ vue2-final-demo/src/views/Home.vue
@@ -1,27 +1,84 @@
 <template>
   <div class="container">
     <van-nav-bar title="标题" fixed/>
-       <h1>aaaa</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>dfdsf</h1>
-       <h1>11111</h1>
+
+  <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <ArtItem
+          v-for="item in artList" 
+          :key="item.id" 
+          :title="item.title"
+          :aut_name="item.aut_name"
+          :comm_count="item.comm_count"
+          :pubdate="item.pubdate"
+          :cover="item.cover"
+        />
+    </van-list>
+  </van-pull-refresh>
+    
+
   </div>
 </template>
 
 <script>
+import request from '../utils/request'
+import ArtItem from '../components/ArtItem.vue' 
+
 export default {
+  name: 'Home',
+  components: {
+    ArtItem
+  },
+  data() {
+    return {
+      page: 0,
+      limit: 10,
+      artList: [],
+      loading: false,
+      finished: false,
+      refreshing: false
+    }
+  },
+  async created() {
+    //解构对象，将data字段赋值给res
+      this.page++
+    const {data: artList} = await request.get('/articles', {
+      params: {
+        _page: this.page,
+        _limit: 10
+      }
+    })
+    this.artList = artList
+  },
+  methods: {
+   async onLoad() {
+       this.page++
+       const {data: artList} = await request.get('/articles', {
+        params: {
+          _page: this.page,
+          _limit: 10
+        }
+      })
+      this.artList = [...this.artList, ...artList]
+      this.loading = false
+      if (artList.length === 0) {
+        this.finished = true
+      }
+    },
+    async onRefresh() {
+      this.artList = []
+      this.page = 0
+      this.finished = false
+      this.loading = true
+      await this.onLoad()
+      this.refreshing = false
+    }
+  },
 
 }
 </script>
